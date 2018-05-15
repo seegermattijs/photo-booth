c28e8622fd58936c5635fac78bf2c9fd1f9a640a
SeMt <seeger.mattijs@ordina.be>
2018-05-15 23:34:06 +0200

    minimize on esc button

--- main.js
+++ main.js
@@ -139,10 +139,10 @@ app.on('activate', function () {
   }
 })
 
-document.addEventListener(“keydown”, event => {
+document.addEventListener('keydown', event => {
 
  switch (event.key) {
-   case “Escape”:
+  case 'Escape':
    if (remote.getCurrentWindow().isFullScreen()) {
      remote.getCurrentWindow().setFullScreen(false);
    }
