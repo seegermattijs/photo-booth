46aaa537903c30a47154ec8b0da3076b26178261
SeMt <seeger.mattijs@ordina.be>
2018-08-11 12:27:35 +0200

    test

--- app/booth.js
+++ app/booth.js
@@ -173,7 +173,7 @@ function trigger() {
                   $('#prompt').fadeOut(250);
                   $( ".save" ).off('click');
                 }
-                executing=false;
+                // executing=false;
               }
 
             } else {
